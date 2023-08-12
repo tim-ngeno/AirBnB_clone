abcc40ea6f01600ceb17c9c88a5ae563b07ab8fe
Tim Ngeno <h34r54y5@gmail.com>
2023-08-12 11:28:59 +0300

    add the amenity module

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Amenity module"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Defines the Amenity class
+
+    Attributes:
+        name (str): name of amenity
+    """
+    name = ""
