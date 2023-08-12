b408549e8e5a0590f576486e120b2b25b576b64e
Mukui012 <spencermukui@gmail.com>
2023-08-12 15:32:10 +0000

    error fix

--- console.py
+++ console.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """Importing modules"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpretor"""
 
     prompt = '(hbnb) '
+
+    classes = {'BaseModel': BaseModel, "User": User, "State": State, "City": City,
+            "Amenity": Amenity, "Place": Place, "Review": Review}
 
     def do_quit(self, arg):
         """Exit the command interpretor"""
@@ -20,6 +31,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Emptyline and ENTER does nothing"""
         pass
+
+    def do_create(self, arg):
+        """ Create an object of any class"""
+        if not arg:
+            print("** class name missing **")
+            return
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[arg]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
 
 
 if __name__ == '__main__':
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""subclass state module"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ Inherits from BaseModel"""
+    name = ""
