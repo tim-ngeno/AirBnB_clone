3ff9451dec2564d766a23274b11ba5685a50e36f
Mukui012 <spencermukui@gmail.com>
2023-08-12 19:16:20 +0000

    test

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""import modules"""
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """tests console"""
+
+    def test_t(self):
+        """test"""
+        pass
+
