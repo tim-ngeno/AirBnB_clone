b4a667050461c56ab727306a10644dec775265b3
Mukui012 <spencermukui@gmail.com>
2023-08-12 09:41:51 +0000

    CLI

--- console.py
+++ console.py
@@ -19,13 +19,5 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-   # def do_create(self, arg):
-    """Creates a new instance of BaseModel and prints its id"""
-    #if not arg:
-     #   print("** class name missing **")
-    #elif arg not in HBNBCommand.classes:
-     #   print("**class doesn't exist**")
-    #else:
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
