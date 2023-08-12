554114c8162fc57d721b4319e60ec198924485a3
Mukui012 <spencermukui@gmail.com>
2023-08-12 10:01:31 +0000

    CLI

--- console.py
+++ console.py
@@ -11,14 +11,14 @@ class HBNBCommand(cmd.Cmd):
         """Exit the command interpretor"""
         return True
 
-    def emptyline(self):
-        """Emptyline + ENTER does nothing"""
-        pass
-
     def do_EOF(self, arg):
         """End of file"""
         print()
         return True
+
+    def emptyline(self):
+        """Emptyline + ENTER does nothing"""
+        pass
 
 
 if __name__ == '__main__':
