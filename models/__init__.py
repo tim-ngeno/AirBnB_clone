531fa1b3667096f5313577253aa3ee390f9b7f19
Tim Ngeno <h34r54y5@gmail.com>
2023-08-12 11:59:02 +0300

    add the __init__ module

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from .engine.file_storage import FileStorage
 
+# Create an instance of the FileStorage class
 storage = FileStorage()
+
+# Call the reload() method on the instance
 storage.reload()
